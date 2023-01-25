8ce2bbe8f822925c071c09d3df85fdd359aed83b
Liam King <techy.willyk3@gmail.com>
2023-01-24 22:36:25 -0600

    I had to create a ne repository as my original project was forked and cloned wrong, which made it be set to private. I still have the original project in case its needed for proof of work and date with original commits.

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -1,28 +1,36 @@
+$(document).ready(readyNow);
+
 // an array to store all of our cars
 let garage = [];
 
-
-$(document).ready(readyNow);
-
 function readyNow() {
   console.log("DOM is loaded!");
-
+  $('#addCar').on('click', addNewCar);
+  // addNewCar();
   // add click event handler that calls the addNewCar function
+};
 
-}
-
-function addNewCar() {
+function addNewCar(){
+  // console.log($('#yearInfo').val(), + $('#makeInfo').val(), + $('#modelInfo').val());
   // get input values
-
+  let newCar = {
+    year: $('#yearInfo').val(),
+    make: $('#makeInfo').val(),
+    model: $('#modelInfo').val()
+  };
   // add to array
-
+    garage.push(newCar);
+    console.log(newCar);
+    console.log('This is my updated garage:', garage);
   // anything else?
+  render();
+};
 
-}
-
-
-
+// I couldn't really figure things out here.
 function render() {
   // update the DOM
-
-}
+  $('#allCars').empty();
+  for (let i=0; i<garage.length; i++ ){
+    $('#allCars').append( `<li> ${garage[i].year} ${garage[i].make} ${garage[i].model} </li>` );
+  }
+};
