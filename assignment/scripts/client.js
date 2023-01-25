b90afff70cfeca49f8ee479b23d1e3a059fbcabf
Will (Liam) King <109176715+wking2022@users.noreply.github.com>
2023-01-24 22:27:51 -0600

    Initial commit

---
+++ assignment/scripts/client.js
@@ -0,0 +1,28 @@
+// an array to store all of our cars
+let garage = [];
+
+
+$(document).ready(readyNow);
+
+function readyNow() {
+  console.log("DOM is loaded!");
+
+  // add click event handler that calls the addNewCar function
+
+}
+
+function addNewCar() {
+  // get input values
+
+  // add to array
+
+  // anything else?
+
+}
+
+
+
+function render() {
+  // update the DOM
+
+}
